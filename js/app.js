0317f3d62c1caec3726b32e123e5eb87528893c1
Porter <porter@Porters-MacBook-Air.local>
2019-10-27 15:53:08 -0300

    Initial POC

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 $(document).foundation();
+
+$(document).ready(function() {
+    // Actions
+    $('#submit').click(function() {
+        $('#begin').hide();
+        $('#exercise').show();
+        fillSelect();
+    });
+
+    $('#back').click(function() {
+        $('#begin').show();
+        $('#exercise').hide();
+    });
+
+    $('#finish').click(function() {
+        $('#begin').show();
+        $('#exercise').hide();
+    });
+
+});
+
+function fillSelect() {
+    $('.select').append(new Option('tu vieja', 'tu vieja'))
+    $('.select').append(new Option('tu vieja', 'tu vieja'))
+    $('.select').append(new Option('tu vieja', 'tu vieja'))
+}
